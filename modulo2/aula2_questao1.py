05ce495ec07e45c44ada381b10dd7f8b5b872040
HenriCh213 <christian.eleoterio@pditabira.com>
2025-02-17 18:46:53 -0300

    Add files via upload

---
+++ modulo2/aula2_questao1.py
@@ -0,0 +1,10 @@
+valor1 = 5
+valor2 = 2
+print(type(valor1))
+print(type(valor2))
+
+print("\n")
+
+valor3 = valor1 / valor2
+print(valor3)
+print(type(valor3))
---
+++ modulo2/aula2_questao2.py
@@ -0,0 +1,8 @@
+resultado = "O resultado é"
+num1 = 10
+num2 = 3.5
+num3 = num1 + num2
+print(resultado)
+print(num1)
+print(num2)
+print(num3)
