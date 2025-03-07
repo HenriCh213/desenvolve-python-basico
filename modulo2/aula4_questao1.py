83a6d17276f83427e629703fa400e33591737d8e
HenriCh213 <christian.eleoterio@pditabira.com>
2025-03-07 19:00:04 -0300

    Add files via upload

---
+++ modulo2/aula4_questao1.py
@@ -0,0 +1,6 @@
+comprimento = int(input("Qual o comprimento?\n"))
+largura = int(input("Qual a largura?\n"))
+preco = float(input("Qual o preço?\n"))
+
+area = int(comprimento * largura)
+print(f"O terreno possui {area}m² e custa R${preco}")
