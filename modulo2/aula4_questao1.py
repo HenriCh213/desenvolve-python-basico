d936d5df5ab3762abbe1f1d281c6e5c61e4883ee
HenriCh213 <christian.eleoterio@pditabira.com>
2025-05-20 07:55:44 -0300

    Update aula4_questao1.py

--- modulo2/aula4_questao1.py
+++ modulo2/aula4_questao1.py
@@ -1,6 +1,14 @@
-comprimento = int(input("Qual o comprimento?\n"))
-largura = int(input("Qual a largura?\n"))
+# É necessário 3 inputs para a resolução do exercício. Sendo eles: Comprimento, largura e preço
+comprimento = float(input("Qual o comprimento?\n"))
+largura = float(input("Qual a largura?\n"))
 preco = float(input("Qual o preço?\n"))
 
-area = int(comprimento * largura)
-print(f"O terreno possui {area}m² e custa R${preco}")
+# A variável 'área' irá armazena o valor da multiplicação entre 'comprimento' e 'largura'
+area = float(comprimento * largura)
+
+# A variável 'preco_total' irá armazena o valor da multiplicação entre 'area' e 'preco' == por conta do preço do terreno ser por área
+preco_total = float(area * preco)
+
+
+# Por fim, imprimimos a área 
+print(f"O terreno possui {area}m² e custa R${preco_total}")
