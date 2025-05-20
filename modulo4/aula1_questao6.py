4bf7d347ee8a2e79f753992e0b6c4de5a4d57794
HenriCh213 <christian.eleoterio@pditabira.com>
2025-05-20 14:53:10 -0300

    Update aula1_questao6.py

--- modulo4/aula1_questao6.py
+++ modulo4/aula1_questao6.py
@@ -6,13 +6,14 @@ quantidade_rato = 0
 
 while vezes_cobaia != 0:
     quantidade = int(input("Informa a quantidade: "))
-    tipo = str(input("Informe o tipo: Sapo, Coelho ou Coelho")).title()
+    tipo = str(input("Informe o tipo: Sapo, Rato ou Coelho\n")).title()
     if tipo == "Sapo":
         quantidade_sapo += quantidade
     elif tipo == "Coelho":
         quantidade_coelho += quantidade
     elif tipo == "Rato":
         quantidade_rato += quantidade
+    vezes_cobaia -= 1
 
 total_cobaias = quantidade_coelho + quantidade_rato + quantidade_sapo
 print(f"Total de cobaias: {total_cobaias}")
@@ -20,9 +21,9 @@ print(f"Total de sapos: {quantidade_sapo}")
 print(f"Total de coelhos: {quantidade_coelho}")
 print(f"Total de ratos: {quantidade_rato}")
 
-percentual_sapo = (quantidade_sapo // total_cobaias) * 100
-percentual_coelho = (quantidade_coelho // total_cobaias) * 100
-percentual_rato = (quantidade_rato // total_cobaias) * 100
+percentual_sapo = round((quantidade_sapo / total_cobaias) * 100, 2)
+percentual_coelho = round((quantidade_coelho / total_cobaias) * 100, 2)
+percentual_rato = round((quantidade_rato / total_cobaias) * 100, 2)
 
 print(f"Percentual do sapo: {percentual_sapo}%")
 print(f"Percentual do rato: {percentual_rato}%")
