f13ce65227a30e8f3efdf0f480ce981149be1653
HenriCh213 <christian.eleoterio@pditabira.com>
2025-05-27 14:05:54 -0300

    Update aula4_questao1.py

--- modulo6/aula4_questao1.py
+++ modulo6/aula4_questao1.py
@@ -1,4 +1,8 @@
 lista_par = [n for n in range(20, 50) if n % 2 == 0]
+print(lista_par)
 lista_quadrado = [n*n for n in range(1, 9)]
+print(lista_quadrado)
 lista_div7 = [n for n in range(1, 100) if n % 7 == 0]
+print(lista_div7)
 lista_div = ["par" if n % 2 == 0 else "impar" for n in range(0, 30, 3)]
+print(lista_div)
